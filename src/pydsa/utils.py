d7e8275e2bd7ea3802a1efcd7d8c88cbb20ca29f
engineer <noemail>
2023-01-04 22:51:52 +0000

    ENH: improve docstrings

--- src/pydsa/utils.py
+++ src/pydsa/utils.py
@@ -1,7 +1,5 @@
 """
-Utilities
----
-This submodule contains utility functions that aid implementing abstract data
+This module ``pydsa.utils`` contains utility functions that aid implementing abstract data
 types and algorithms.
 """
 
@@ -12,17 +10,25 @@ __all__ = ('py_obj_array_type', 'ElemTypeName', 'validate_close_range')
 
 
 ElemTypeName = Literal['int', 'uint', 'float', 'double', 'object']
+"""All possible options for contiguous memory cell item type.
+"""
 
 
 def py_obj_array_type(size: int, item_type: ElemTypeName = None) -> Type:
-    """
-    Creates and returns a type that represents contiguous memory cells for
-    ``n`` objects of ``item_type``.
+    """Creates and returns a type that represents contiguous memory cells for
+    ``size`` number of objects of ``item_type``.
 
-    Parameters
-    ----------
-    item_type : ElemTypeName
-        Array item type -- 'int' | 'uint' | 'float' | 'double' | 'object'
+    Args:
+        item_type (ElemTypeName): Cell item type -- 'int', 'uint', 'float', 'double', 'object'.
+
+    Raises:
+        ValueError: if ``item_type`` is not one of allowed options.
+
+    Returns:
+        _type_: The contiguous memory cell type desired.
+
+    See Also:
+        ``pydsa.utils.ElemTypeName``
     """
     if item_type is not None:
         if item_type == 'int':
@@ -41,15 +47,31 @@ def py_obj_array_type(size: int, item_type: ElemTypeName = None) -> Type:
 
 
 def is_pos_power_of_two(num: int) -> bool:
-    """
-    Returns ``True`` if ``n`` is a positive power of 2.
+    """Checks if ``num`` is a positive power of 2.
+
+    Args:
+        num (int): The number to test.
+
+    Returns:
+        bool: ``True`` if ``num`` is a positive power of 2, ``False`` otherwise.
     """
     return num > 0 and (num & (num - 1) == 0)
 
 
 def validate_close_range(name: str, value: int, low: int, high: int,
                          error: Type[Exception] = IndexError) -> None:
-    """Raises ``error`` if ``value`` falls out of the close range ``[low, high].
+    """Checks if the number ``value`` falls within the close range ``[low, 
+    high]``.
+
+    Args:
+        name (str): Name of the number to check.
+        value (int): The number to check. 
+        low (int): Beginning of the close range.
+        high (int): End of the close range.
+        error (Type[Exception], optional): The exception type to raise when the test fails. 
+
+    Raises:
+        error: if ``value`` falls outside the close range ``[low, high]``.
     """
     if value < low or value > high:
         raise error(f'{name}={value} out of range [{low}, {high}]')
